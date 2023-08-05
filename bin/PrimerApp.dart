e3dabf951e7946634a56e86fe6d3acc42ef6cfc2
Oscar Eliseo Torres Vasquez <oscar2004torres@outlook.es>
2023-08-05 10:44:00 -0600

    first commit

---
+++ bin/PrimerApp.dart
@@ -0,0 +1,15 @@
+void main(List<String> arguments) {
+  //variable nombre
+  var nombre = 'oscar';
+  //variable edad
+  var edad = 19;
+  //variable apellido
+  var primerApellido = 'torres';
+  
+  /*imprimir en pantalla nombre edad y el primer apellido
+  tambien imprimimos el tipo del valor de la variable 
+   */
+  print(nombre.runtimeType);
+  print(edad.runtimeType);
+  print(primerApellido.runtimeType);
+}
